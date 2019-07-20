2357a9aad919d3bc0a7702fb5f7ff38454f42943
user <user@gmail.com>
2019-07-20 09:21:38 +0530

    Updates

---
+++ precache-manifest.3d9e30e30058f260b1f612bdca8dd01c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c0b266311cb95b89ced690588cfb76",
+    "url": "/Student_list/index.html"
+  },
+  {
+    "revision": "5a97b2fa3dece6178560",
+    "url": "/Student_list/static/css/main.ee76450c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Student_list/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5a97b2fa3dece6178560",
+    "url": "/Student_list/static/js/main.ce0e9138.chunk.js"
+  },
+  {
+    "revision": "1ce5fc11feb9a4d41b31",
+    "url": "/Student_list/static/js/runtime~main.a4f8cbba.js"
+  }
+]);
